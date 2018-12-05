ed07d186e0956bf3938eecfd3071fb74c38b4d36
yyj <yyj@jyy.com>
2018-12-05 16:41:59 +0800

    init

---
+++ app/src/main/java/pri/common/LoadLayout.java
@@ -0,0 +1,198 @@
+package com.baidu.iov.dueros.film.view;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.support.annotation.DrawableRes;
+import android.support.annotation.IntDef;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.annotation.StringRes;
+import android.support.v7.widget.RecyclerView;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.baidu.iov.dueros.film.R;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@SuppressWarnings({"FieldCanBeLocal", "unused"})
+public class LoadLayout extends FrameLayout {
+
+    private View mAddable;
+    private ViewGroup mEmpty;
+    private ViewGroup mError;
+    private View mNetNotWork;
+    private View mLoading;
+    private View mMask;
+
+    @NonNull
+    private View mContent;
+
+    private int mCurrentState = -1;
+
+    public LoadLayout(@NonNull Context context) {
+        this(context, null);
+    }
+
+    public LoadLayout(@NonNull Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public LoadLayout(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+
+        TypedArray a = context.getTheme().obtainStyledAttributes(attrs, R.styleable.LoadLayout, 0, 0);
+
+        int addableId = a.getResourceId(R.styleable.LoadLayout_load_addable, R.layout.view_loadlayout_addable);
+        int emptyId = R.layout.view_loadlayout_empty;
+        int errorId = R.layout.view_loadlayout_error;
+        int netnotworkId = a.getResourceId(R.styleable.LoadLayout_load_netnotwork, R.layout.view_loadlayout_netnotwork);
+        int loadingId = a.getResourceId(R.styleable.LoadLayout_load_loading, R.layout.view_loadlayout_loading);
+        int contentId = a.getResourceId(R.styleable.LoadLayout_load_content, R.layout.view_loadlayout_content);
+
+        LayoutInflater inflater = LayoutInflater.from(getContext());
+
+        inflater.inflate(addableId, this, true);
+        inflater.inflate(emptyId, this, true);
+        inflater.inflate(errorId, this, true);
+        inflater.inflate(netnotworkId, this, true);
+        inflater.inflate(loadingId, this, true);
+        inflater.inflate(contentId, this, true);
+        inflater.inflate(R.layout.view_loadlayout_mask, this, true);
+
+        mAddable = getChildAt(0);
+        mEmpty = (ViewGroup) getChildAt(1);
+        mError = (ViewGroup) getChildAt(2);
+        mNetNotWork = getChildAt(3);
+        mLoading = getChildAt(4);
+        mContent = getChildAt(5);
+        // Mask must be the last one
+        mMask = getChildAt(6);
+
+        a.recycle();
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+
+        for (int i = 0, count = getChildCount(); i < count; i++) {
+            View view = getChildAt(i);
+            view.setVisibility(GONE);
+        }
+    }
+
+    /**
+     * @throws IllegalStateException do not set load_content if you wanna get a default value
+     */
+    @NonNull
+    public RecyclerView getDefaultContent() {
+        if (mContent instanceof RecyclerView) {
+            return (RecyclerView) mContent;
+        } else {
+            throw new IllegalStateException("you should not set 'load_content' in XML file");
+        }
+    }
+
+    public View getContent() {
+        return mContent;
+    }
+
+    public void setMask(boolean boom) {
+        mMask.setVisibility(boom ? VISIBLE : GONE);
+    }
+
+    public void setEmpty(EmtpyBuider buider) {
+        if (buider.iconId > 0) {
+            ((ImageView) mEmpty.getChildAt(0)).setImageResource(buider.iconId);
+        }
+        if (buider.textId > 0) {
+            ((TextView) mEmpty.getChildAt(1)).setText(buider.textId);
+        }
+    }
+
+    public void setError(ErrorBuider buider) {
+        if (buider.iconId > 0) {
+            ((ImageView) mError.getChildAt(0)).setImageResource(buider.iconId);
+        }
+        if (buider.textId > 0) {
+            ((TextView) mError.getChildAt(1)).setText(buider.textId);
+        }
+        mError.getChildAt(2).setOnClickListener(buider.listener);
+    }
+
+    public void setState(@State int state) {
+        if (mCurrentState == state)
+            return;
+
+        mCurrentState = state;
+        // not contain mask
+        for (int i = 0, count = getChildCount() - 1; i < count; i++) {
+            View view = getChildAt(i);
+            if (i == state) {
+                view.setVisibility(VISIBLE);
+            } else {
+                view.setVisibility(GONE);
+            }
+        }
+    }
+
+    @IntDef({State.Addable, State.Empty, State.Error, State.NetNotWork, State.Loading, State
+            .Content})
+    @Retention(RetentionPolicy.SOURCE)
+    public @interface State {
+        int Addable = 0x00000000;
+        int Empty = 0x0000001;
+        int Error = 0x00000002;
+        int NetNotWork = 0x00000003;
+        int Loading = 0x00000004;
+        int Content = 0x0000005;
+        int Mask = 0xffffffff;
+    }
+
+    public static class EmtpyBuider {
+        @DrawableRes
+        private int iconId;
+        @StringRes
+        private int textId;
+
+        public EmtpyBuider icon(@DrawableRes int iconId) {
+            this.iconId = iconId;
+            return this;
+        }
+
+        public EmtpyBuider text(@StringRes int textId) {
+            this.textId = textId;
+            return this;
+        }
+    }
+
+    public static class ErrorBuider {
+        @DrawableRes
+        private int iconId;
+        @StringRes
+        private int textId;
+        private View.OnClickListener listener;
+
+        public ErrorBuider icon(@DrawableRes int iconId) {
+            this.iconId = iconId;
+            return this;
+        }
+
+        public ErrorBuider text(@StringRes int textId) {
+            this.textId = textId;
+            return this;
+        }
+
+        public ErrorBuider listener(View.OnClickListener listener) {
+            this.listener = listener;
+            return this;
+        }
+    }
+}
